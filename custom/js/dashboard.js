3c52303524905b43285771e3a7ed6b3bf3fbe18f
eduardocoutinho <eduardocesar93@hotmail.com>
2017-05-28 10:11:02 -0300

    adjusts in code style

--- custom/js/dashboard.js
+++ custom/js/dashboard.js
@@ -1,21 +1,21 @@
-var modalId = -1; 
+var modalId = -1;
 
-var adjustElements = function(){
+var adjustElements = function() {
     $rows = $('.table-editable tbody tr th');
     totalLength = $rows.length;
-    for (var i = 0; i < totalLength; i++){
+    for (var i = 0; i < totalLength; i++) {
         $('.table-editable tbody tr th:eq(' + i + ')').html(i);
     }
 };
 
-var bSort = function(){
+var bSort = function() {
     $rows = $('.table-editable tbody tr th');
     totalLength = $rows.length;
-    for (var i = 0; i < totalLength; i++){
-        for (var j = 0; j < (totalLength - i - i); j++){
+    for (var i = 0; i < totalLength; i++) {
+        for (var j = 0; j < (totalLength - i - i); j++) {
             if (parseInt($('.table-editable tbody tr th:eq(' + j + ')').html()) >
                 parseInt($('.table-editable tbody tr th:eq(' + (j + 1) + ')').html())
-            ){
+            ) {
                 temp = $('.table-editable tbody tr:eq(' + j + ')').html();
                 $('.table-editable tbody tr:eq(' + j + ')').html(
                     $('.table-editable tbody tr:eq(' + (j + 1) + ')').html()
@@ -27,15 +27,15 @@ var bSort = function(){
     addEvents();
 };
 
-var addEvents = function(){
-    $('.table-remove').click(function () {
+var addEvents = function() {
+    $('.table-remove').click(function() {
         modalId = $(this).parents('tr').index();
         $name = $('.table-editable tbody tr:eq(' + modalId + ') td:eq(0)');
         $('.modal-name').html($name.html())
         $('#myModal').modal('toggle');
     });
 
-    $('.table-up').click(function () {
+    $('.table-up').click(function() {
         var $row = $(this).parents('tr');
         if ($row.index() === 0) return;
         $row.prev().before($row.get(0));
@@ -44,7 +44,7 @@ var addEvents = function(){
         $('.save-list').show();
     });
 
-    $('.table-down').click(function () {
+    $('.table-down').click(function() {
         var $row = $(this).parents('tr');
         if ($row.parent().children().length == $row.index() + 1) return;
         $row.next().after($row.get(0));
@@ -53,15 +53,15 @@ var addEvents = function(){
         $('.save-list').show();
     });
 
-    $('.save-list').click(function(){
+    $('.save-list').click(function() {
         adjustElements();
         $('.span-save').hide();
         $('.cancel-list').hide();
         $('.save-list').hide();
     });
-    
-    
-    $('.cancel-list').click(function(){
+
+
+    $('.cancel-list').click(function() {
         bSort();
         $('.span-save').hide();
         $('.cancel-list').hide();
@@ -69,39 +69,31 @@ var addEvents = function(){
     });
 };
 
-var QueryString = function () {
-  // This function is anonymous, is executed immediately and 
-  // the return value is assigned to QueryString!
-  var query_string = {};
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-        // If first entry with this name
-    if (typeof query_string[pair[0]] === "undefined") {
-      query_string[pair[0]] = decodeURIComponent(pair[1]);
-        // If second entry with this name
-    } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
-      query_string[pair[0]] = arr;
-        // If third or later entry with this name
-    } else {
-      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+var QueryString = function() {
+    var query_string = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (typeof query_string[pair[0]] === "undefined") {
+            query_string[pair[0]] = decodeURIComponent(pair[1]);
+        } else if (typeof query_string[pair[0]] === "string") {
+            var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+            query_string[pair[0]] = arr;
+        } else {
+            query_string[pair[0]].push(decodeURIComponent(pair[1]));
+        }
     }
-  } 
-  return query_string;
+    return query_string;
 }();
 
 addEvents();
-$('.confirm-delete').click(function(){
+$('.confirm-delete').click(function() {
     $row = $('.table-editable tbody tr:eq(' + modalId + ')');
     $row.detach();
     $('#myModal').modal('toggle');
 });
 
-
-if (QueryString.success && QueryString.success == "true"){
+if (QueryString.success && QueryString.success == "true") {
     $(".alert").show();
 }
-
-
